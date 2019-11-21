adc4f3644c816c70658647b58bd677ec2358ee54
Ryan Brown <ryan.brown@barre3.com>
2019-11-21 15:27:55 -0800

    iinitial commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 2,
+            bacon: 1,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
