938cf49ce9a86e00cb4300c038af881b728ee3fd
Ryan Brown <ryan.brown@barre3.com>
2019-11-21 16:18:22 -0800

    adadd the order button

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -7,9 +7,11 @@ const buildControl = (props) => (
     <div className={classes.Label}>{props.label}</div>
     <button
       className={classes.Less}
-    >Less</button>
+      onClick={props.removed}
+      disabled={props.disabled}>Less</button>
     <button
       className={classes.More}
+      onClick={props.added}
     >More</button>
   </div>
 );
