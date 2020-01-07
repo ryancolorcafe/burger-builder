ce2a56873881d63bd603d954a327ace1f63998a3
Ryan Brown <ryan.brown@barre3.com>
2020-01-06 16:56:14 -0800

    add price total to orderSummary.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,9 +90,10 @@ class BurgerBuilder extends Component {
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary
-                        ingredients={this.state.ingredients} 
+                        ingredients={this.state.ingredients}
+                        price={this.state.totalPrice}
                         purchaseCanceled={this.purchaseCancelHandler}
-                        purchaseContinued={this.purchaseContinueHandler}/>
+                        purchaseContinued={this.purchaseContinueHandler} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
