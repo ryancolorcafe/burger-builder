0de83661594520875a1bcc63fa3303d57c5e649a
Ryan Brown <ryan.brown@barre3.com>
2020-03-04 16:44:44 -0800

    add Firebase DB, withErrorHandler HOC, spinner

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -7,11 +7,11 @@ import Backdrop from '../Backdrop/Backdrop';
 class Modal extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.show !== this.props.show;
+    return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
   }
 
-  componentWillUpdate() {
-    console.log('[Modal] WillUpdate');
+  componentDidUpdate() {
+    console.log('[Modal] DidUpdate');
   }
 
   render() {
